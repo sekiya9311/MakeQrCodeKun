96aa5224b0115f415d36798dd4b9b29eed0051b1
sekiya9311 <sekiya9311@gmail.com>
2019-12-17 22:00:30 +0900

    Add project files.

---
+++ MakeQrCodeKun/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using MakeQrCodeKun.Views;
+using System.Windows;
+
+namespace MakeQrCodeKun
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
